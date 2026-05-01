1231fc19f322289b5adde52947d511834ae2cf33
Flex <flex@example.com>
2026-05-01 16:19:32 -0500

    feat: unified dnd.bat control script (start/stop/restart/status) + fix ESM __dirname

--- src/utils/locale-loader.ts
+++ src/utils/locale-loader.ts
@@ -1,6 +1,9 @@
 import fs from "fs";
 import path from "path";
+import { fileURLToPath } from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const LOCALES_DIR = path.join(__dirname, "../../locales");
 
 // Cache loaded locale data
